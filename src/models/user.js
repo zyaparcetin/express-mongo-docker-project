38ac1593d112d6b7b37fcc2f0d4ddccc9a403f5f
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-04 19:42:02 +0000

    Edit some lines

--- src/models/user.js
+++ src/models/user.js
@@ -1,49 +1,7 @@
 const mongoose = require('mongoose')
 const autopopulate = require('mongoose-autopopulate')
-
-const userSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    unique: true,
-    required: true,
-  },
-  age: {
-    type: Number,
-    required: true,
-  },
-  bio: String,
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-})
-
-class User {
-  greet(person) {
-    console.log(`Hello ${person.name}, this is ${this.name}`)
-  }
-
-  get profile() {
-    return `
-# ${this.name} (${this.age})
-Bio: ${this.bio}
-    `
-  }
-
-  set profile(newValue) {
-    throw new Error(`profile is only a getter. You can't override it with ${newValue}.`)
-  }
-}
-
-userSchema.loadClass(User)
-userSchema.plugin(autopopulate)
-
-module.exports = mongoose.model('User', userSchema)
-
-// my user.js
-/* const mongoose = require('mongoose')
-const autopopulate = require('mongoose-autopopulate')
 const Review = require('./review')
+const Product = require('./product')
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -101,7 +59,8 @@ class User {
     product.reviews.push(review)
     await this.save()
     await review.save()
-  } */
+  }
+}
 
 /* get profile() { // Armagan's code
     return `
@@ -119,3 +78,47 @@ class User {
 userSchema.plugin(autopopulate)
 
 module.exports = mongoose.model('User', userSchema) */
+
+// in template:
+/* const mongoose = require('mongoose')
+const autopopulate = require('mongoose-autopopulate')
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    unique: true,
+    required: true,
+  },
+  age: {
+    type: Number,
+    required: true,
+  },
+  bio: String,
+  createdAt: {
+    type: Date,
+    default: new Date(),
+  },
+})
+
+class User {
+  greet(person) {
+    console.log(`Hello ${person.name}, this is ${this.name}`)
+  }
+
+  get profile() {
+    return `
+# ${this.name} (${this.age})
+Bio: ${this.bio}
+    `
+  }
+
+  set profile(newValue) {
+    throw new Error(`profile is only a getter. You can't override it with ${newValue}.`)
+  }
+}
+
+userSchema.loadClass(User)
+userSchema.plugin(autopopulate)
+
+module.exports = mongoose.model('User', userSchema) 
+*/
