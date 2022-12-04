c4e3aac482b7592d66c7ef9d9bb364ff80661651
zyaparcetin <lavinia.zeynep35@gmail.com>
2022-12-04 19:22:14 +0000

    Edit some lines, delete some lines

--- src/routes/products.js
+++ src/routes/products.js
@@ -1,36 +1,23 @@
-// const express = require('express')
-// const { products } = require('../models')
+const express = require('express')
+const Product = require('../models/product')
+const User = require('../models/user')
+// const Review = require('../models/review')
 
-// const router = express.Router()
+const router = express.Router()
 
 /* GET products listing. */
-/* router.get('/', (req, res, next) => {
+router.get('/', async (req, res) => {
   const query = {}
-  res.send(await User.find(query).catch(error => console.log('Users not found, error: ', error))) // users.js'teki gibi yazmaya calistim, hata verdi
-}) */
-//  res.send(products)
-/* GET initialize */
-/* router.get('/initialize', async (req, res) => {
-  const mihri = await User.create({ name: 'mihri', age: 35 })
-  const armagan = await User.create({ name: 'armagan', age: 36 })
-
-  const steve = await User.create({ name: 'steve', age: 21 })
-  steve.bio = 'An awesome hacker who has seen it all, and now sharing them all with you.'
-
-  steve.greet(mihri)
-  steve.greet(armagan)
-
-  console.log(steve)
-  res.sendStatus(200)
-}) */
-
-/* POST user */
-/* router.post('/', async (req, res) => {
-  const createdUser = await User.create(req.body)
-  res.status(201).send(createdUser)
+  res.send(await Product.find(query).catch(error => console.log('Products not found, error: ', error)))
 })
 
-module.exports = router */
+/* POST product */
+router.post('/', async (req, res) => {
+  const createdProduct = await Product.create(req.body)
+  res.status(201).send(createdProduct)
+})
+
+module.exports = router
 
 /* from my project
 
@@ -42,22 +29,4 @@ router.get('/:productId', (req, res, next) => {
       message: `No product with id ${req.params.id} found`,
     })
   return res.send(product)
-}) */
-
-// module.exports = router
-
-/* router.get('/:name', (req, res) => {
-  const user = users.find(u => u.name === req.params.name)
-
-  if (!user)
-    return res.render('error', {
-      error: { status: 404 },
-      message: `No user with name ${req.params.name} found`,
-    })
-
-  return res.send(user)
-})
-
-router.get('/:userId', (req, res) => {
-  return res.send(users[req.params.userId])
 }) */
